942954173fc52c3216174f5b78133bc9b03712c2
phov0 <37254379+phov0@users.noreply.github.com>
2018-03-12 22:14:44 -0300

    Add files via upload

---
+++ Scripts/LevelControlManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelControlManager : MonoBehaviour {
+
+    public int sceneIndex, levelPassed;
+    
+
+    // Use this for initialization
+    void Start () {
+       
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+	
+	}
+	
+	public void youWin()
+    {
+        if (levelPassed < sceneIndex)
+        {
+            PlayerPrefs.SetInt("LevelPassed", sceneIndex);
+        }
+    }
+
+    public void SceneIndex()
+    {
+        sceneIndex = sceneIndex++;
+    }
+}
